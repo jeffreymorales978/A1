e35cab1563ab422e02238bc014f3d0d66e0dc6a9
jeffreymorales978 <jeffmorayo@gmail.com>
2022-10-25 21:11:50 -0500

    version2

---
+++ suma.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+ float x,y,z;
+ cout<<"Ingrese 2 numero x y :";
+ cin>>x>>y;
+ z=x+y;
+ cout<<"El resutado es : ";
+ cout<<z<<endl;
+
+ return 0;
+
+}
